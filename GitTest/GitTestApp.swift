478e555ba127378cdc9fb364ad7dbca6bd5de7ce
Ian Bailey <IanKulin@kulin.com.au>
2022-07-31 10:42:46 +0800

    Initial

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Ian Bailey on 31/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
